e80c48f90d8a79363e574c97b94f5d171e1d0ac3
Er Anees Akbar <99726488+eranees@users.noreply.github.com>
2025-06-24 12:46:35 +0530

    Delete backend directory

--- backend/src/mediasoup/mediasoup.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MediasoupService } from './mediasoup.service';
-import { SignalingGateway } from './signaling.gateway';
-
-@Module({
-  providers: [MediasoupService, SignalingGateway],
-})
-export class MediasoupModule {}
